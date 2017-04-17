b87cf67e99d27c2386d3425d9100a61fad4fd73e
Samuel Pavon <festovalros@gmail.com>
2017-04-17 00:31:05 -0400

    initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,16 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+festovalros:$1$Al71rf5y$egww5QLBvZ61cTJxgwoub1:Samuel Pavon:festovalros@gmail.com:admin,user
+wilmeragsucv:$1$EI6WrOxZ$lHqaLft53GlU4neqp7U5I/:Wilmer:wilmer.a.gonzalez@ucv.ve:admin,user
+guachusperquin:$1$938i8YIW$ITNlbJ1fOZeU8vL.dJ9.v1:Marcos Rubén García:guachusperquin@gmail.com:admin,user
+agomez:$1$Eu1wOx0T$uUTRZqXB8PC.yZv8sIPWa0:Alberto Gomez:agomez@micorp.com.ve:user
+hectorgrand:$1$e7abC7wo$PS6pqLhOZXn3xJ.lAU0lc.:Hector Grand:hector.grand.m@gmail.com:user
